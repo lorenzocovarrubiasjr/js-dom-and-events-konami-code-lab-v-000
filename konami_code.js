50c45615a9506e0c70732a476d858013a24c0ce1
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-03-06 02:27:19 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
-let keys = [];
 
 function init(e) {
   // Write your JavaScript code inside the init() function
@@ -9,10 +8,9 @@ function init(e) {
   console.log(key)
   debugger;
   if (key === code[index]) {
-    keys.push(key)
     index++
     if (index === code.length){
-        (keys === code) ? (alert("You cracked the code!"), index = 0, keys = []) : (index = 0, keys = []);
+        alert('You cracked the code, dawg! Congratulations!!!')
     }
     else {
       init(e)
