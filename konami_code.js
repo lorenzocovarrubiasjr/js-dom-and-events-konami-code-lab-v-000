4a90e44d1898d12cd3d97d90bb1c6336f4d37077
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-03-06 02:08:05 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,12 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
+let keys = [];
 
 function init(e) {
   // Write your JavaScript code inside the init() function
   const key = parseInt(e.which);
-    keys = [];
+
     console.log(key)
   if (key === code[index]) {
     keys.push(key)
