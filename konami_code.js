6e79aa422d3c8614aac865792a0f42691421cdac
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-03-06 02:30:21 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,7 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
-debugger;
 function init(e) {
   // Write your JavaScript code inside the init() function
   const key = parseInt(e.which || e.detail);
