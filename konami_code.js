74416214458819145a9085a908641d54b0ea346c
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-03-06 02:02:11 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+let index = 0;
 
+function init(e) {
+  // Write your JavaScript code inside the init() function
+  const key = parseInt(e.which);
+
+  if (key === code[index]) {
+    console.log(key)
+    index++
+    if (index === code.length){
+      alert("You cracked the code!")
+
+      index = 0;
+    }
+    else {
+      key = NaN;
+      init(e)
+    }
+  }
 }
+
+
+window.addEventListener('keydown', init)
